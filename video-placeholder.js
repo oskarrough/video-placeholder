f843c1203553466cdc475647fe78e8e0c7dbaf84
Oskar Rough <oskar@rough.dk>
2018-08-08 15:55:27 +0200

    Clean up

--- video-placeholder.js
+++ video-placeholder.js
@@ -7,50 +7,31 @@ class VideoPlaceholder extends HTMLElement {
     this.front = this.querySelector('video-placeholder-front')
     this.back = this.querySelector('template').content
 
-		// const shouldOpen = this.getAttribute('open') !== null
-    // if (shouldOpen) this.open()
-
     this.addEventListener('click', this.click)
   }
 
   click(event) {
-    console.log(event)
-
     if (event.target.className === 'KwVideo-close') {
-      console.log('tap->close')
-      this.close()
-      return
-    }
-
-    if (!this.getAttribute('open')) {
-      console.log('tap->open')
-      this.open()
-      return
+      this.removeAttribute('open')
+    } else if (!this.getAttribute('open')) {
+      this.setAttribute('open', true)
     }
   }
 
-  open() {
-    this.setAttribute('open', true)
+  // Attributes that trigger callback below.
+  static get observedAttributes() {
+    return ['open']
   }
-
-  close() {
-    this.removeAttribute('open')
-  }
-
-	// Attributes that trigger callback below.
-	static get observedAttributes() {
-		return ['open']
-	}
 
   attributeChangedCallback(name, oldValue, newValue) {
-		console.log({name, oldValue, newValue})
+    console.log({name, oldValue, newValue})
 
-		if (name === 'open' && newValue !== null && newValue !== 'false') {
-			if (!this.wasOpened) {
-				this.appendChild(this.back.cloneNode(true))
-			}
-			this.wasOpened = true
-		}
+    if (name === 'open' && newValue !== null && newValue !== 'false') {
+      if (!this.wasOpened) {
+        this.appendChild(this.back.cloneNode(true))
+      }
+      this.wasOpened = true
+    }
   }
 }
 
